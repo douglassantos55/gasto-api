3496e37286e685315a908674d3909574439b95e6
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-01-05 19:50:24 -0300

    feat: hash password on edit

--- src/repositories/sequelize/users.ts
+++ src/repositories/sequelize/users.ts
@@ -3,8 +3,16 @@ import { randomUUID } from "crypto"
 import Repository from "./repository"
 import UserModel from "../../models/user"
 import { User, UserCreationData } from "../../types"
+import { Condition } from "../types"
 
 class UserRepository extends Repository<User> {
+    async update(data: User, condition: Condition<User>) {
+        if (data.password) {
+            data.password = await bcrypt.hash(data.password, 10)
+        }
+        return super.update(data, condition)
+    }
+
     async create(data: UserCreationData): Promise<User> {
         return super.create({
             ...data,
