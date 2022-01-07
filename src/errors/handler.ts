0f6466df60db334d7860521f626411a8774a3910
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-01-07 16:36:05 -0300

    refactor: move error handler

---
+++ src/errors/handler.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express"
+import { AuthenticationError, UploadError, ValidationError } from "./"
+
+export default function(err: Error, _req: Request, res: Response, next: NextFunction) {
+    if (res.headersSent) {
+        return next(err)
+    }
+
+    if (err instanceof ValidationError) {
+        return res.status(400).json(err.errors)
+    } else if (err instanceof UploadError) {
+        return res.status(400).json({ [err.file]: err.message })
+    } else if (err instanceof AuthenticationError) {
+        return res.status(403).end(err.message)
+    } else {
+        return res.status(500).end(err.message)
+    }
+}
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv"
-import express, { NextFunction, Request, Response } from "express"
+import express from "express"
 import users from "./routes/users"
 import auth from "./routes/auth"
 import expenses from "./routes/expenses"
-import { AuthenticationError, UploadError, ValidationError } from "./errors"
+import errorHandler from "./errors/handler"
 
 dotenv.config()
 
@@ -15,21 +15,6 @@ app.use(express.json())
 app.use("/users", users)
 app.use("/auth", auth)
 app.use("/expenses", expenses)
-
-app.use(function(err: Error, _req: Request, res: Response, next: NextFunction) {
-    if (res.headersSent) {
-        return next(err)
-    }
-
-    if (err instanceof ValidationError) {
-        return res.status(400).json(err.errors)
-    } else if (err instanceof UploadError) {
-        return res.status(400).json({ [err.file]: err.message })
-    } else if (err instanceof AuthenticationError) {
-        return res.status(403).end(err.message)
-    } else {
-        return res.status(500).end(err.message)
-    }
-})
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
