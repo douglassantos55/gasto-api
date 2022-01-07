fa21d4b1335512116ca4b1ec784ba3c6e107177d
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-01-07 18:45:35 -0300

    feat: cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import express from "express"
+import cors from "cors"
 import users from "./routes/users"
 import auth from "./routes/auth"
 import expenses from "./routes/expenses"
@@ -10,6 +11,7 @@ dotenv.config()
 const port = 3000
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/users", users)
