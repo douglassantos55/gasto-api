f3c6595fa3be17676c8719e545c5c8131d925ecd
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-01-09 11:28:32 -0300

    feat: update limits

--- src/routes/limits.ts
+++ src/routes/limits.ts
@@ -7,10 +7,40 @@ const router = Router()
 
 router.get("/", authMiddleware, async (req: Request, res: Response, next: NextFunction) => {
     try {
-        return res.json(await repository.all({ user_id: req.user.id }))
+        return res.json(await repository.all({
+            ...req.query,
+            user_id: req.user.id
+        }))
     } catch (err) {
         next(err)
     }
+})
+
+router.put("/", authMiddleware, async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const data = req.body
+
+        await validator.validate(data, {
+            total: validator.rules().required().numeric(),
+            month: validator.rules().required().numeric(),
+            year: validator.rules().required().numeric(),
+        })
+
+        // Remove previous register (if any) and then create a new one
+        await repository.destroy({
+            month: data.month,
+            year: data.year,
+            user_id: req.user.id
+        })
+
+        res.json(await repository.create({
+            ...data,
+            user_id: req.user.id
+        }))
+    } catch (err) {
+        next(err)
+    }
+
 })
 
 router.post("/", authMiddleware, async (req: Request, res: Response, next: NextFunction) => {
