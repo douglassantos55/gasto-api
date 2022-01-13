ebb6eeee3b37a948412e40698c220c67ef7c17f2
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-01-13 16:14:38 -0300

    build: more postgres stuff

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -1,17 +1,27 @@
-import { Dialect, Sequelize } from "sequelize"
+import { Dialect, Options, Sequelize } from "sequelize"
+
+let options: Options = {
+    logging: false,
+    dialect: process.env.DATABASE_DIALECT as Dialect,
+    host: process.env.DATABASE_HOST,
+    storage: process.env.DATABASE_STORAGE,
+    port: process.env.DATABASE_PORT as unknown as number,
+}
+
+if (process.env.DATABASE_DIALECT === "postgres") {
+    options.dialectOptions = {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+}
 
 const conn = new Sequelize(
     process.env.DATABASE_NAME,
     process.env.DATABASE_USERNAME,
     process.env.DATABASE_PASSWORD,
-    {
-        logging: false,
-        dialect: process.env.DATABASE_DIALECT as Dialect,
-        host: process.env.DATABASE_HOST,
-        storage: process.env.DATABASE_STORAGE,
-        port: process.env.DATABASE_PORT as unknown as number,
-        dialectOptions: { ssl: true }
-    }
+    options
 )
 
 conn.sync()
